b98c36771e03b5544f7caae070dcc42cc276169b
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-01 10:01:54 +0300

    update 02 and 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+let data = {};
+
+const fillData = function () {
+  const savedData = localStorage.getItem('feedback-form-state');
+  const parseData = JSON.parse(savedData);
+  data = parseData;
+  const keys = Object.keys(data);
+  keys.forEach(key => {
+    const input = formRef.elements[key];
+    input.value = data[key];
+  });
+};
+
+fillData();
+
+const onInput = function (e) {
+  const input = e.target;
+  data[input.name] = input.value;
+  const strData = JSON.stringify(data);
+  localStorage.setItem('feedback-form-state', strData);
+};
+
+const onSubmit = function (e) {
+  e.preventDefault();
+
+  console.log(data);
+  
+  e.currentTarget.reset();
+  data = {};
+  localStorage.removeItem('feedback-form-state');
+};
+formRef.addEventListener('input', throttle(onInput, 500));
+formRef.addEventListener('submit', onSubmit);
