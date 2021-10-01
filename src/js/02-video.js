395f12bde4cedac1df58edbc1887c7aa455860c5
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-01 10:29:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,6 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-// Разбери документацию метода on() и начни отслеживать событие timeupdate - 
-// обновление времени воспроизведения.
-// Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для 
-// хранилища будет строка "videoplayer-current-time".
-// При перезагрузке страницы воспользуйся методом setCurrentTime() для того 
-// чтобы возобновить воспроизведение с сохраненной позиции.
-// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения 
-// обновлялось в хранилище не чаще чем раз в секунду.
-
-
 player.on('timeupdate', throttle(data =>
     {
         localStorage.setItem("videoplayer-current-time", data.seconds)
